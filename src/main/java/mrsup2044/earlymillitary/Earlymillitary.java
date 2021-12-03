0e9abecb22fbd20cee0f804c1b2c7605a764b16f
Wobee13 <wobee13@gmail.com>
2021-12-02 20:42:58 -0600

    Added A test Item

--- src/main/java/mrsup2044/earlymillitary/Earlymillitary.java
+++ src/main/java/mrsup2044/earlymillitary/Earlymillitary.java
@@ -1,10 +1,18 @@
 package mrsup2044.earlymillitary;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Earlymillitary implements ModInitializer {
+    // an instance of our new item
+    public static final Item FABRIC_ITEM = new Item(new FabricItemSettings().group(ItemGroup.COMBAT));
+
     @Override
     public void onInitialize() {
-
+        Registry.register(Registry.ITEM, new Identifier("tutorial", "fabric_item"), FABRIC_ITEM);
     }
 }
