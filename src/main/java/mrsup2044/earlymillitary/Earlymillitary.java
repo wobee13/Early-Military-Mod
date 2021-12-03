cda2324105b8b46f32f2a15207ab9691efb7bb65
Wobee13 <wobee13@gmail.com>
2021-12-02 20:21:40 -0600

    Initial Commit

---
+++ src/main/java/mrsup2044/earlymillitary/Earlymillitary.java
@@ -0,0 +1,10 @@
+package mrsup2044.earlymillitary;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Earlymillitary implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/mrsup2044/earlymillitary/client/EarlymillitaryClient.java
@@ -0,0 +1,13 @@
+package mrsup2044.earlymillitary.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class EarlymillitaryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
