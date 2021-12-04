b0c42f6fb7a10d2bfba04a60cee22cdd5a2bba58
Wobee13 <wobee13@gmail.com>
2021-12-03 22:45:41 -0600

    Made Ammo Crate

--- src/main/java/mrsup2044/earlymilitary/EarlyMilitary.java
+++ src/main/java/mrsup2044/earlymilitary/EarlyMilitary.java
@@ -16,10 +16,10 @@ import net.minecraft.util.registry.Registry;
 public class EarlyMilitary implements ModInitializer {
     // an instance of our new item
 
-    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier("tutorial", "general"), () -> new ItemStack(Blocks.GOLD_BLOCK));
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier("earlymilitary", "general"), () -> new ItemStack(Blocks.GOLD_BLOCK));
 
     public static final ItemGroup OTHER_GROUP = FabricItemGroupBuilder.create(
-                    new Identifier("tutorial", "other"))
+                    new Identifier("earlymilitary", "other"))
             .icon(() -> new ItemStack(Items.GOLDEN_SHOVEL))
             .appendItems(stacks -> {
                 stacks.add(new ItemStack(Blocks.BONE_BLOCK));
@@ -30,7 +30,7 @@ public class EarlyMilitary implements ModInitializer {
             })
             .build();
 
-    public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+    public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.of(Material.WOOD).strength(4.0f));
 
 
     public static final Item FABRIC_ITEM = new Item(new FabricItemSettings().group(EarlyMilitary.ITEM_GROUP));
@@ -38,9 +38,9 @@ public class EarlyMilitary implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("tutorial", "fabric_item"), FABRIC_ITEM);
-        Registry.register(Registry.ITEM, new Identifier("test","test1"), Another_ITEM);
-        Registry.register(Registry.BLOCK, new Identifier("tutorial", "example_block"), EXAMPLE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("tutorial", "example_block"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(EarlyMilitary.ITEM_GROUP)));
+        Registry.register(Registry.ITEM, new Identifier("earlymilitary", "fabric_item"), FABRIC_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("earlymilitary","test1"), Another_ITEM);
+        Registry.register(Registry.BLOCK, new Identifier("earlymilitary", "ammo_crate"), EXAMPLE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("earlymilitary", "ammo_crate"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(EarlyMilitary.ITEM_GROUP)));
     }
 }
